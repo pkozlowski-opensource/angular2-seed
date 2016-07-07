333b0b519a25f7a65fc499d914b9215c7e63a798
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-07 20:13:05 +0200

    install demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { NGB_DIRECTIVES, NGB_PRECOMPILE } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app',
   pipes: [],
   providers: [],
-  directives: [ ROUTER_DIRECTIVES ],
+  directives: [ ROUTER_DIRECTIVES, NGB_DIRECTIVES ],
+  precompile: [ NGB_PRECOMPILE ],
   templateUrl: './app.html',
 })
 export class App {
